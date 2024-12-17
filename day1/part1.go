e786e89a3adffeab2dfce49cc0cca89b13cdbbf3
Exc0mmun1cad0 <fauxmen4@gmail.com>
2024-12-17 20:58:09 +0300

    part 1 solution

---
+++ day1/part1.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+const (
+	inputFile = "input.txt"
+	linesCnt  = 1e3
+	sep       = "   "
+)
+
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
+func PartOneSolution() {
+	f, _ := os.Open(inputFile)
+	scanner := bufio.NewScanner(f)
+	nums1, nums2 := make([]int, 0, linesCnt), make([]int, 0, linesCnt)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		numsInLine := strings.Split(line, sep) // always 2 nums in line
+
+		num, _ := strconv.Atoi(numsInLine[0])
+		nums1 = append(nums1, num)
+		num, _ = strconv.Atoi(numsInLine[1])
+		nums2 = append(nums2, num)
+	}
+
+	// Sorting nums in both slices
+	sort.Ints(nums1)
+	sort.Ints(nums2)
+	// Make sure that they are of the same length
+	if len(nums1) != len(nums2) {
+		log.Fatal("Slices are of different size. Something went wrong...")
+	}
+
+	totalDist := 0
+	for i := 0; i < len(nums1); i++ {
+		totalDist += (abs(nums2[i] - nums1[i]))
+	}
+
+	fmt.Println(totalDist)
+}
